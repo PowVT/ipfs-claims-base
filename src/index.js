b92196cf21197ba7b4035206432d959b49051342
PowVT <epowlowsky@gmail.com>
2021-12-09 09:49:59 -0500

    data loaded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,116 @@
 const makeIpfsFetch = require('ipfs-fetch')
 
+// Claim Parameters
+let commodity;
+let prodID;
+let mass;
+let purity;
+let certification;
+let creatorLocation;
+let image;
+let timestamp;
+let supportDirectives;
+let associatedAddresses;
+
 // function get data
 function getClaimData(hash) {
     console.log(hash);
-    const res = await fetch(hash);
-    const text = await res.text();
-    console.log(text);
+    try {
+        // get data from IPFS/ Infura
+        const res = await fetch(hash);
+        const text = await res.text();
+        console.log(text);
+        let splitSpace = text.split('\n')
+
+        // fill claim parameters
+        //commodity
+        commodity = splitSpace[3].split(":")[1]
+        console.log("Commodity: ", commodity)
+        //productID
+        prodID = splitSpace[5].split(":")[1].valueOf();
+        console.log("ProductID: ", prodID)
+        //mass
+        mass = splitSpace[7].split(":")[1].valueOf();
+        console.log("Mass: ", mass)
+        //purity
+        let purityWhole = splitSpace[9].split(":")[1]
+        let pure = purityWhole.split(",")
+        for (let i = 0; i < pure.length; i++) {
+            let element = pure[i].split(")")[0] + ")";
+            let percent = pure[i].split(")")[1]
+            purity[element] = percent;
+        }
+        console.log("Purity: ", purity)
+        //certification
+        let certWhole = splitSpace[11].split(":")[1];
+        let cert = certWhole.split(" ");
+        let certNum = cert[cert.length - 1];
+        let certName;
+        if (cert[cert.length - 2]) {
+            let certName3 = cert[cert.length - 2];
+            certName = certName3;
+        }
+        if (cert[cert.length - 3]) {
+            let certName2 = cert[cert.length - 3];
+            if (certName2) {
+                certName = certName2 + " " + certName;
+            }
+        }
+        if (cert[cert.length - 4]) {
+            let certName1 = cert[cert.length - 4];
+            if (certName1) {
+                certName = certName1 + " " + certName;
+            }
+        }
+        certification[certName] = certNum
+        //creator location
+        creatorLocation = splitSpace[13].split(":")[1]
+        console.log("Creator Location: ", creatorLocation)
+        //image
+        image = splitSpace[15].split(":")[2]
+        console.log("Image: ", "https:" + image)
+        //timestamp
+        console.log("Timestamp: ", splitSpace[17].split(":")[1] + ":" + splitSpace[17].split(":")[2] + ":" + splitSpace[17].split(":")[3])
+        //minesite directives
+        if (splitSpace[20]) {
+            supportDirectives[1] = splitSpace[20].split("          ")[1]
+        }
+        if (splitSpace[21]) {
+            supportDirectives[2] = splitSpace[21].split("          ")[1]
+        }
+        if (splitSpace[22]) {
+            supportDirectives[3] = splitSpace[22].split("          ")[1]
+        }
+        console.log("Support Directives: ", supportDirectives)
+        //wallet addresses
+        if (splitSpace[25]) {
+            let name1 = splitSpace[25].split(":")[0];
+            let name1S = name1.split("          ")[1];
+            let address1 = splitSpace[25].split(":")[1];
+            associatedAddresses[name1S] = address1
+        }
+        if (splitSpace[26]) {
+            let name2 = splitSpace[26].split(":")[0];
+            let name2S = name2.split("         ")[1];
+            let address2 = splitSpace[26].split(":")[1];
+            associatedAddresses[name2S] = address2
+        }
+        if (splitSpace[27]) {
+            let name3 = splitSpace[27].split(":")[0];
+            let name3S = name3.split("         ")[1];
+            let address3 = splitSpace[27].split(":")[1];
+            associatedAddresses[name3S] = address3
+        }
+        console.log("Addresses Attached to Claim: ", associatedAddresses)
+        console.log("\n");
+
+    } catch (e) {
+        console.error(e)
+    }
+
+
 }
+
 // function length of data?
 
 // function total value?
--- test/test.js
+++ test/test.js
@@ -10,17 +10,21 @@ test('Load a file from the CID and log result:', async function (t) {
     let commodity;
     let prodID;
     let mass;
-    let purity;
-    let certification;
+    let purity = {};
+    let certification = {};
     let creatorLocation;
     let image;
     let timestamp;
-    let supportDirectives;
-    let associatedAddresses;
+    let supportDirectives = {};
+    let associatedAddresses = {};
 
     try {
         const response = await fetch('https://ipfs.infura.io:5001/api/v0/cat?arg=' + correctHash)
         const text = await response.text()
+        // fetch response tests
+        t.ok(response, 'Got a response object.')
+        t.equal(response.status, 200, 'Got status 200 in fetch response.')
+        // remove empty lines
         let splitSpace = text.split('\n')
 
         console.log("\n");
@@ -34,24 +38,84 @@ test('Load a file from the CID and log result:', async function (t) {
         mass = splitSpace[7].split(":")[1].valueOf();
         console.log("Mass: ", mass)
         //purity
-        console.log("Purity: ", splitSpace[9].split(":")[1])
-        console.log("certification: ", splitSpace[11].split(":")[1])
-        console.log("creatorLocation: ", splitSpace[13].split(":")[1])
-        console.log("image: ", "https:" + splitSpace[15].split(":")[2])
-        console.log("timestamp: ", splitSpace[17].split(":")[1])
-        console.log("supportDirectives: ", splitSpace[20], splitSpace[21], splitSpace[22])
-        console.log("associatedAddresses: ", splitSpace[25], splitSpace[26], splitSpace[27])
+        let purityWhole = splitSpace[9].split(":")[1]
+        let pure = purityWhole.split(",")
+        for (let i = 0; i < pure.length; i++) {
+            let element = pure[i].split(")")[0] + ")";
+            let percent = pure[i].split(")")[1]
+            purity[element] = percent;
+        }
+        console.log("Purity: ", purity);
+        //certification
+        let certWhole = splitSpace[11].split(":")[1];
+        let cert = certWhole.split(" ");
+        let certNum = cert[cert.length - 1];
+        let certName;
+        if (cert[cert.length - 2]) {
+            let certName3 = cert[cert.length - 2];
+            certName = certName3;
+        }
+        if (cert[cert.length - 3]) {
+            let certName2 = cert[cert.length - 3];
+            if (certName2) {
+                certName = certName2 + " " + certName;
+            }
+        }
+        if (cert[cert.length - 4]) {
+            let certName1 = cert[cert.length - 4];
+            if (certName1) {
+                certName = certName1 + " " + certName;
+            }
+        }
+        certification[certName] = certNum
+        console.log("Certification: ", certification)
+        //creator location
+        creatorLocation = splitSpace[13].split(":")[1]
+        console.log("Creator Location: ", creatorLocation)
+        //image
+        image = splitSpace[15].split(":")[2]
+        console.log("Image: ", "https:" + image)
+        //timestamp
+        timestamp = splitSpace[17].split(":")[1] + ":" + splitSpace[17].split(":")[2] + ":" + splitSpace[17].split(":")[3]
+        console.log("Timestamp: ", timestamp)
+        //minesite directives
+        if (splitSpace[20]) {
+            supportDirectives[1] = splitSpace[20].split("          ")[1]
+        }
+        if (splitSpace[21]) {
+            supportDirectives[2] = splitSpace[21].split("          ")[1]
+        }
+        if (splitSpace[22]) {
+            supportDirectives[3] = splitSpace[22].split("          ")[1]
+        }
+        console.log("Support Directives: ", supportDirectives)
+        //wallet addresses
+        if (splitSpace[25]) {
+            let name1 = splitSpace[25].split(":")[0];
+            let name1S = name1.split("          ")[1];
+            let address1 = splitSpace[25].split(":")[1];
+            associatedAddresses[name1S] = address1
+        }
+        if (splitSpace[26]) {
+            let name2 = splitSpace[26].split(":")[0];
+            let name2S = name2.split("         ")[1];
+            let address2 = splitSpace[26].split(":")[1];
+            associatedAddresses[name2S] = address2
+        }
+        if (splitSpace[27]) {
+            let name3 = splitSpace[27].split(":")[0];
+            let name3S = name3.split("         ")[1];
+            let address3 = splitSpace[27].split(":")[1];
+            associatedAddresses[name3S] = address3
+        }
+        console.log("Addresses Attached to Claim: ", associatedAddresses)
         console.log("\n");
-
-        // fetch response
-        t.ok(response, 'Got a response object.')
-        t.equal(response.status, 200, 'Got status 200 in fetch response.')
-
-        // mass greater than 0
-        t.ok(Number(mass) > 0, 'Mass is greater than 0.')
 
     } catch (e) {
         console.error(e)
     }
+
+    // mass greater than 0
+    t.ok(Number(mass) > 0, 'Mass is greater than 0.')
 
 })
